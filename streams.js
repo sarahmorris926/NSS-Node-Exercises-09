9d24de091b383868ed039101cca332584aaa9049
Sarah Morris <sarah.morris9@gmail.com>
2018-03-09 09:38:53 -0600

    Completed exercise

--- streams.js
+++ streams.js
@@ -1,7 +1,29 @@
 #!/usr/bin/env node
-'use strict';
+const readFile = process.argv[2];
+const destFile = process.argv[3];
 
-const { readFile } = process.argv[2];
-const { destFile } = process.argv[3];
+const {createReadStream, createWriteStream, writeFile} = require("fs")
 
-const { createReadStream } = require('');
+const {Transform, Writable} = require("stream");
+
+const capitalize = Transform();
+const writeStream = Writable();
+
+capitalize._transform = (buffer, _, cb) => {
+  cb(null, buffer.toString().toUpperCase())
+}
+
+writeStream._write = (buffer, _, next) => {
+  writeFile(destFile, buffer, (error) => {
+    if(error) throw error;
+    console.log("Woohooooooo it's done!");
+  })
+  next();
+}
+
+createReadStream(readFile)
+.pipe(capitalize)
+.pipe(writeStream)
+
+
+
